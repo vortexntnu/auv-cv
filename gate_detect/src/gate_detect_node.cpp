62d6160b016baee63909b8fe0b475ef3fb6f2aa9
Thomas Hellum <thomas.s.hellum@gmail.com>
2019-01-08 14:23:39 +0100

    Inital ROS CV

---
+++ gate_detect/src/gate_detect_node.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  CV
+//
+//  Created by Thomas Hellum on 22/10/2018.
+//  Copyright © 2018 Thomas Hellum. All rights reserved.
+//
+
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/tracking.hpp>
+#include <opencv2/core/ocl.hpp>
+
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm> 
+
+
+using namespace cv;
+using namespace std;
+
+int main() {
+	
+
+}	
+
+
+
+int main() {
+
+}	
